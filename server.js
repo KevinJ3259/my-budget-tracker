c26e2dbb7a28aefc89cd6a07b0a5f93d67d5433b
KevinJ3259 <kmjordan2009.kj@gmail.com>
2022-08-01 20:13:32 -0500

    commit

--- server.js
+++ server.js
@@ -16,12 +16,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-});
+mongoose.connect(MONGODB_URI);
 
-// routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
